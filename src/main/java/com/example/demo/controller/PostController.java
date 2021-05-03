ae5b710d81c69e34639263d15f084c6d891de722
Carlos Padilla <mr.carlos.padilla@gmail.com>
2021-05-03 13:32:10 -0500

    completed views exercise

--- src/main/java/com/example/demo/controller/PostController.java
+++ src/main/java/com/example/demo/controller/PostController.java
@@ -1,25 +1,31 @@
 package com.example.demo.controller;
 
+import com.example.demo.models.Post;
 import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestMethod;
-import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.ArrayList;
+import java.util.List;
 
 @Controller
 public class PostController {
 
-
-    @RequestMapping(path = "/posts", method = RequestMethod.GET)
-    @ResponseBody
-    public String posts(){
-        return "posts index page";
+    @GetMapping("/posts")
+    public String allPosts(Model model){
+        List<Post> posts =  new ArrayList<>();
+        posts.add(new Post(1, "title1", "body1"));
+        posts.add(new Post(2, "title2", "body2"));
+        posts.add(new Post(3, "title3", "body3"));
+        model.addAttribute("allPosts", posts);
+        return "posts/index";
     }
 
-    @RequestMapping(path = "/posts/{id}", method = RequestMethod.GET)
-    @ResponseBody
-    public String individualPost(@PathVariable long id){
-        return "viewing post with id of " + id;
+    @GetMapping("/posts/{id}")
+    public String individualPost(@PathVariable long id, Model model) {
+        Post post = new Post(id, "test-title", "test-body");
+        model.addAttribute("post", post);
+        return "posts/show";
     }
 
     @RequestMapping(path = "/posts/create", method = RequestMethod.GET)
