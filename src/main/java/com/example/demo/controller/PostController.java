05ad6ffbbcf503d305e4cc4a4a5361f3ce5726de
Carlos Padilla <mr.carlos.padilla@gmail.com>
2021-05-12 09:24:37 -0500

    feat: changes to views

--- src/main/java/com/example/demo/controller/PostController.java
+++ src/main/java/com/example/demo/controller/PostController.java
@@ -73,6 +73,7 @@ public class PostController {
     @GetMapping("/posts/{id}/show")
     public String showDetails(@PathVariable long id, Model vModel) {
         Post postToView = postDao.getOne(id);
+
 //        PostDetails postDetails = postToView.getPostDetails();
         vModel.addAttribute("post", postToView);
 //        vModel.addAttribute("posts", postToView);
